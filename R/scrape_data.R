1602e9a8cea5fcd86e67c7a0e5533b137e9961c8
nrennie <nrennie35@gmail.com>
2024-08-25 20:50:59 +0100

    update data

--- R/scrape_data.R
+++ R/scrape_data.R
@@ -9,12 +9,40 @@ str_extract_between <- function(x, start, end) {
 }
 
 a_char <- romeo_juliet_raw |> 
-  html_elements("a") |> 
+  html_elements("h3, a") |> 
   as.character()
 
 script <- tibble(raw_char = a_char) |> 
   # Remove top two title rows
   slice(-c(1, 2)) |> 
+  # What Act is it?
+  mutate(
+    act = case_when(
+      str_detect(raw_char, "ACT") ~ str_extract_between(raw_char, "<h3>", "</h3>"),
+      TRUE ~ NA_character_
+    ),
+    act = str_replace(act, "ACT", "Act")
+  ) |> 
+  mutate(
+    act_line = str_detect(raw_char, "ACT")
+  ) |> 
+  fill(act, .direction = "down") |> 
+  filter(!act_line) |> 
+  select(-act_line) |>
+  # What Scene is it?
+  mutate(
+    scene_line = str_detect(raw_char, "SCENE"),
+    scene = str_extract_between(raw_char, "<h3>", "</h3>"),
+    scene = str_replace(scene, "SCENE", "Scene"),
+    scene = str_replace(scene, "PROLOGUE", "Prologue"),
+    scene = case_when(
+      str_detect(scene, "\\.") ~ sub("\\..*", "", scene),
+      TRUE ~ scene
+    )
+  ) |> 
+  fill(scene, .direction = "down") |> 
+  filter(!scene_line) |> 
+  select(-scene_line) |>
   # Who is speaking?
   mutate(
     character = case_when(
@@ -39,6 +67,7 @@ script <- tibble(raw_char = a_char) |>
   mutate(
     character = replace_na(character, "Chorus")
   ) |> 
+  drop_na(dialogue) |> 
   mutate(line_number = row_number()) |> 
   mutate(
     character = case_when(
