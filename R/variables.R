3beca4a3a74e2b9e84415f1b2a8cd8f87e7d24e3
nrennie <nrennie35@gmail.com>
2024-08-25 20:19:09 +0100

    update variable names

--- R/variables.R
+++ R/variables.R
@@ -1,26 +1,11 @@
 # List of characters
 male_names <- c(
-  "Mike", "Jim", "Dustin", "Lucas", "Will", "Ted", "Troy",
-  "Ben", "Mr. Clarke", "Hopper", "Jonathan", "Callahan", "Lonnie",
-  "Powell", "Steve", "Tommy", "Donald", "Earl", "Ronald Reagan",
-  "O'Bannon", "Shepard", "Pastor Charles", "Dr. Brenner", "Reed",
-  "Axel", "Mick", "Keith", "Murray", "Merrill", "Owens", "Bob",
-  "Jack", "Billy", "Teddy", "Mr. Sinclair", "Terry", "Ray",
-  "Funshine", "Neil", "Sam", "Bruce", "Tom", "Larry", "Grigori",
-  "Alexei", "Todd", "Marty", "Doc", "Ten", "Argyle", "Eddie",
-  "Fred", "Enzo", "Wayne", "Sullivan", "Daniels", "Gareth", "Jason",
-  "Patrick", "Antonov", "Hatch", "Harmon", "Victor", "Brenner",
-  "Andy", "Wallace", "Cornelius", "Yuri", "Tanner", "Tatum", "Two",
-  "One", "Louis Armstrong", "Armstrong"
+  "Sampson", "Gregory", "Abraham", "Benvolio", "Tybalt", "Capulet",
+  "Montague", "Prince", "Romeo", "Paris", "Mercutio", "Friar Laurence", 
+  "Peter", "Balthasar", "Friar John",
+  "Page", "First Watchman", "Second Watchman", "Third Watchman"
 )
 
 female_names <- c(
-  "Eleven", "Nancy", "Joyce", "Barb", "Max", "Liz", "Karen",
-  "Barbara", "Florence", "Carol", "Holly", "Cynthia", "Sandra",
-  "Ms. Holland", "Nicole", "Becky", "Connie", "Sarah", "Dottie",
-  "Tina", "Marsha", "Mrs. Sinclair", "Erica", "Claudia", "Lynn",
-  "Kali", "Susan", "Diane", "Mrs. Driscoll", "Robin", "Flo",
-  "Heather", "Candice", "Erica", "Suzie", "Mrs. Ergenbright",
-  "Angela", "Mrs. Gracey", "Chrissy", "Kelley", "Alice", "Eden",
-  "Mrs. Henderson", "Mrs. Wheeler", "Virginia", "Vickie", "Fitzgerald"
+  "Lady Capulet", "Lady Montague", "Juliet", "Nurse"
 )
