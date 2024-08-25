7aea85d7b79cf77c04598be37ecf5a184afefa24
nrennie <nrennie35@gmail.com>
2024-08-25 20:19:33 +0100

    update targets file

--- _targets.R
+++ _targets.R
@@ -1,13 +1,17 @@
 # _targets.R file
 library(targets)
-source("R/functions.R")
+
+# source files
 source("R/variables.R")
+source("R/bechdel.R")
 source("R/plots.R")
+
+# targets
 tar_option_set(packages = c("readr", "dplyr", "ggplot2", "tidyr", "purrr", "tibble"))
 list(
-  tar_target(file, "data/dialogue.csv", format = "file"),
+  tar_target(file, "data/script.csv", format = "file"),
   tar_target(raw_data, get_data(file)),
-  tar_target(tests, test_all_episodes(raw_data, female_names, male_names)),
+  tar_target(tests, bechdel_test(raw_data, female_names, male_names)),
   tar_target(overall_plot_output, overall_plot(tests)),
   tar_target(individual_plot_output, individual_plot(tests))
 )
