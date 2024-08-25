b1d74508f248a7a4061a5e53e5449a358987c8b2
nrennie <nrennie35@gmail.com>
2024-08-25 17:17:50 +0100

    convert to targets pipeline

---
+++ R/plots.R
@@ -0,0 +1,56 @@
+# Heat map of season and episode all pass
+overall_plot <- function(all_tests) {
+  g <- all_tests |>
+    select(season, episode, overall) |>
+    mutate(overall = factor(overall, levels = c("TRUE", "FALSE"))) |> 
+    ggplot() +
+    geom_tile(aes(x = factor(episode), y = factor(season), fill = overall)) +
+    scale_fill_manual(values = c("TRUE" = "grey", "FALSE" = "blue"),
+                      drop = FALSE) +
+    labs(x = "Episode", y = "Season") +
+    scale_y_discrete(limits = rev) +
+    coord_cartesian(expand = FALSE) +
+    theme_minimal() +
+    theme(
+      legend.position = "top",
+      legend.title = element_blank()
+    )
+  ggsave("results/overall_plot.png", width = 8, height = 4, bg = "white")
+}
+
+# Heat map of three tests and episode and season along bottom
+individual_plot <- function(all_tests) {
+  all_tests |>
+    mutate(
+      episode_label = paste0("Season ", season, " Episode ", episode)
+    ) |>
+    select(episode_label, test1, test2, test3) |>
+    pivot_longer(
+      -episode_label,
+      names_to = "test", values_to = "value"
+    ) |>
+    mutate(value = factor(value, levels = c("TRUE", "FALSE"))) |> 
+    ggplot() +
+    geom_tile(
+      aes(x = episode_label, y = test, fill = value)
+    ) +
+    scale_fill_manual(values = c("TRUE" = "grey", "FALSE" = "blue"),
+                      drop = FALSE) +
+    scale_y_discrete(
+      labels = c(
+        "test1" = "There are at least two named women",
+        "test2" = "who speak to each other",
+        "test3" = "about something other than a man"
+      ),
+      limits = rev
+    ) +
+    labs(x = NULL, y = NULL) +
+    coord_cartesian(expand = FALSE) +
+    theme_minimal() +
+    theme(
+      axis.text.x = element_text(angle = 90),
+      legend.position = "top",
+      legend.title = element_blank()
+    )
+  ggsave("results/individual_plot.png", width = 8, height = 4, bg = "white")
+}
---
+++ _targets.R
@@ -0,0 +1,13 @@
+# _targets.R file
+library(targets)
+source("R/functions.R")
+source("R/variables.R")
+source("R/plots.R")
+tar_option_set(packages = c("readr", "dplyr", "ggplot2", "tidyr", "purrr", "tibble"))
+list(
+  tar_target(file, "data/dialogue.csv", format = "file"),
+  tar_target(raw_data, get_data(file)),
+  tar_target(tests, test_all_episodes(raw_data, female_names, male_names)),
+  tar_target(overall_plot, overall_plot(tests)),
+  tar_target(individual_plot, individual_plot(tests))
+)
